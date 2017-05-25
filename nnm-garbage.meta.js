876d16445b9ec1b4e9a0a44a753d02f1a557c090
Nicolas Angelov <nikovolt220@gmail.com>
2017-05-26 00:15:42 +0300

    Create nnm-garbage.meta.js

---
+++ nnm-garbage.meta.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         NNM Mod Delete
+// @namespace    NNMModDelete
+// @version      0.01
+// @description  Добавляет кнопки для  переноса в мусорку быстро и надёжно. И чекбоксы, для множественного переноса.
+// @author       NIK220V
+// @match        http://nnmclub.to/forum/viewtopic.php?*
+// @match        https://nnmclub.to/forum/viewtopic.php?*
+// @match        http://*.nnmclub.to/forum/viewtopic.php?*
+// @match        https://*.nnmclub.to/forum/viewtopic.php?*
+// @match        http://nnm-club.me/forum/viewtopic.php?*
+// @match        https://nnm-club.me/forum/viewtopic.php?*
+// @match        http://*.nnm-club.me/forum/viewtopic.php?*
+// @match        https://*.nnm-club.me/forum/viewtopic.php?*
+// @match        http://nnm-club.i2p.onion/forum/viewtopic.php?*
+// @match        https://nnm-club.i2p.onion/forum/viewtopic.php?*
+// @match        http://nnmclub5toro7u65.onion/forum/viewtopic.php?*
+// @match        https://nnmclub5toro7u65.onion/forum/viewtopic.php?*
+// @homepage     https://github.com/NIK220V/nnm-club-garbage
+// @updateURL    https://github.com/NIK220V/nnm-club-garbage/raw/master/nnm-garbage.meta.js
+// @downloadURL  https://github.com/NIK220V/nnm-club-garbage/raw/master/nnm-garbage.user.js
+// @grant        none
+// ==/UserScript==
