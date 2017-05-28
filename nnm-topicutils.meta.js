b03b0763f91c9fbfb6b6c40d89a502339801fda8
Nicolas Angelov <nikovolt220@gmail.com>
2017-05-28 16:18:56 +0300

    Create nnm-topicutils.meta.js

---
+++ nnm-topicutils.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         NNM Topic Utils
+// @namespace    NNMTopicUtils
+// @version      0.02
+// @description  Полезные функции для модерирования в топиках. Подробнее в README.MD
+// @author       NIK220V
+// @match        *://*.nnmclub.to/forum/viewtopic.php?*
+// @match        *://*.nnm-club.me/forum/viewtopic.php?*
+// @match        *://*.nnm-club.i2p.onion/forum/viewtopic.php?*
+// @match        *://*.nnmclub5toro7u65.onion/forum/viewtopic.php?*
+// @homepage     https://github.com/NIK220V/nnm-club-topicutils
+// @updateURL    https://github.com/NIK220V/nnm-club-topicutils/raw/master/nnm-topicutils.meta.js
+// @downloadURL  https://github.com/NIK220V/nnm-club-topicutils/raw/master/nnm-topicutils.user.js
+// @grant        none
+// ==/UserScript==
